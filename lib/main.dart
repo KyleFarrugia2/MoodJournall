9a1450cad1a84b89788891a5e54849ff1f900772
KyleFarrugia2 <kylef@example.com>
2026-01-24 19:14:26 +0100

    MOODJOURNAL PUBLISH

--- lib/main.dart
+++ lib/main.dart
@@ -43,10 +43,10 @@ class MyApp extends StatelessWidget {
             brightness: Brightness.light,
           ),
           useMaterial3: true,
-          cardTheme: CardTheme(
+          cardTheme: const CardThemeData(
             elevation: 2,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+              borderRadius: BorderRadius.all(Radius.circular(16)),
             ),
           ),
           appBarTheme: const AppBarTheme(
